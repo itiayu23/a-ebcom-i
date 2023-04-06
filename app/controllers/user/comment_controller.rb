95eada91abc0904d6c7c549bd32661ae77a7781f
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-04-06 10:00:57 +0000

    comment bookmark controller ADD

--- app/controllers/user/comment_controller.rb
+++ app/controllers/user/comment_controller.rb
@@ -1,2 +1,23 @@
 class User::CommentController < ApplicationController
-end
+
+  def create
+    @novel = Novel.find(params[:novel_id])
+    @pict = Pict.find(params[:pict_id])
+    @comment = current_user.comment.new(novel_id: @novel.id, pict_id: @pict.id)
+    @comment.save
+  end
+
+    def destroy
+    @novel = Novel.find(params[:novel_id])
+    @pict = Pict.find(params[:pict_id])
+    Comment.find(params[:id]).destroy
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
+  end
