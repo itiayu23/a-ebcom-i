64e830bca3898451a30b5cbf290a9479d19a27ff
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-04-15 12:09:15 +0000

    DM

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@
     # resources :bookmarks, only: [:index]
     resources :novels do
       resource :bookmarks, only: [:create, :destroy]
-      resource :comments, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
     end
 
     resources :picts do
