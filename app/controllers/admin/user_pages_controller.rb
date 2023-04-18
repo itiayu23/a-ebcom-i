5342ecc9574ec47d1a9b9656f138aaee067727bd
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-04-18 10:04:55 +0000

    Admin user controllerAdd

--- app/controllers/admin/user_pages_controller.rb
+++ app/controllers/admin/user_pages_controller.rb
@@ -1,10 +1,33 @@
 class Admin::UserPagesController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @users = User.all.page(params[:page]).per(5)
   end
 
   def show
+    @users = User.all
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    flash[:notice] = "変更を保存しました"
+    redirect_to admin_user_page_path(@user.id)
+    else
+    render :edit
+    end
+  end
+  
+  private
+  
+  def user_params
+  params.require(:user).permit(:nickname, :profile_image, :name, :birthday, :profile, :contact, :twitter, :facebook, :instagram)
+  end
+  
 end
