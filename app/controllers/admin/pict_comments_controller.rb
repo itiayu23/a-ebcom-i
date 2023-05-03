92500f5f48195269ac6b782b2d0bad4336633197
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-05-03 13:19:57 +0000

    api language  add

--- app/controllers/admin/pict_comments_controller.rb
+++ app/controllers/admin/pict_comments_controller.rb
@@ -2,4 +2,11 @@ class Admin::PictCommentsController < ApplicationController
   def index
     @pict_comments = PictComment.all
   end
+
+  private
+
+  def pict_comment_params
+    params.require(:pict_comment).permit(:comment)
+  end
+
 end
